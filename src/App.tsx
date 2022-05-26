02b2b33d98446eb9df9a185bdb85a288f86414bc
Moamin <moamin.salamah@gmail.com>
2022-05-26 11:12:32 +0200

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import React, { FC, useState, ChangeEvent } from 'react';
+import './App.css';
+import { ITask } from './Interfaces';
+import TodoTask from './components/TodoTask'
+
+const App: FC = () => {
+
+  const [task, setTask] = useState<string>('');
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {   //void is to avoid error for not returning a value for the function
+    if (event.target.name === 'task') {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    // console.log(todoList);
+    setTask('');
+    setDeadline(0);
+  };
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(todoList.filter((task) => {
+      return task.taskName !== taskNameToDelete;
+    }))
+  };
+
+
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <span>TypeScript Todo List</span>
+          <input
+            type="text"
+            placeholder='Add a task'
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder='Deadline in days'
+            name='deadline'
+            value={deadline}
+            onChange={handleChange} />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
